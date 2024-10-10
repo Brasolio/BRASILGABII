ea8c4467c0d9a6e84322e40d76e6fca7b2e97175
Brasolio <miguel.nossabein@escola.pr.gov.br>
2024-10-10 08:35:18 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,127 @@
+criaCartao(
+    'Matemática',
+    'Qual número vem depois do nove?',
+     'Dez'
+  )
+  
+  criarCartao(
+    'Biologia',
+    'Quantos pares de cromosomos tem um humano?',
+    '23 pares'
+  )
+  
+    criarCartao(
+    'Projeto de vida',
+    'Voce tem currículo?',
+    'Nao'
+  )
+  
+    criarCartao(
+    'Geografia',
+    'Quais sao os 3 planaltos do estado do Paraná?',
+    'Curitiba, Ponta Grossa e Guarapuava'
+  )
+  
+    criarCartao(
+    'Filosofia',
+    'Qual filosofo disse a frase só sei que nada sei?',
+    'Sócrates'
+  )
+  
+    criarCartao(
+    'Química',
+    'Qual é a fórmula da água?',
+    'H2O'
+  )
+  
+    criarCartao(
+    'Física',
+    'Qual é o nome da medida de temperatura nos Estados Unidos?',
+    'Graus Fahrenheit'
+  )
+  
+    criarCartao(
+    'Portugues',
+    'Qual é a época do romantismo?',
+    'Século 18 e 19'
+  ) 
+  
+    criarCartao(
+    'Astronomia',
+    'Qual é o nome do maior planeta do sistema solar?',
+    'Júpiter'
+  )
+  
+    criarCartao(
+    'Biologia',
+    'Qual é o animal com 6 sentidos?',
+    'Ornitirrinco'
+  )
+  
+    criarCartao(
+    'Hitória',
+    'Qual é a data da queda da Bastilha?',
+    '14/07/1789'
+  )     
+  
+    criarCartao(
+    'Física',
+    'Qual é o nome do satélite natural da Terra?',
+    'Lua'
+  )
+  
+    criarCartao(
+    'Robótica',
+    'Qual é o cabo que usamos para conectar o arduíno no computador?',
+    'Cabo USB'
+  )
+  
+   criarCartao(
+    'Biologia',
+    'qual maior orgão humano?',
+    'Pele'
+  )
+  
+   criarCartao(
+    'Matemática',
+    'Quanto é 5 mais 5?',
+    '10'
+  )
+  
+  criarCartao(
+    'Química',
+    'O que é o H na tabela periódica?',
+    'Hidrogenio'
+  )
+  
+  criarCartao(
+    'História',
+    'Qual o dia da independencia do Brasil?',
+    'Sete de setembro'
+  )
+  
+  criarCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de 25?',
+    '5'
+  )
+  
+  criarCartao(
+    'Geografia',
+    'Qual a capital do Paraná?',
+    'Curitiba'
+  )
+  
+  criarCartao(
+    'Matemática',
+    'Quanto é 6 mais 6?',
+    '12'
+  )
+  
+  
+  
+  
+  
+  
+  
+  
